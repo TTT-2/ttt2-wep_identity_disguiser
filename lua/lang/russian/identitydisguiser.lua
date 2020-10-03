35b45d566848313d2649151924ce4ef6fb6011a4
Blueberryy <igory.ygr200@gmail.com>
2020-10-03 12:08:34 +0500

    Create identitydisguiser.lua

---
+++ lua/lang/russian/identitydisguiser.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+L["weapon_indentiy_disguiser_name"] = "Маскировка личности"
+L["weapon_indentiy_disguiser_desc"] = "Выясните личность другого игрока или труп, чтобы представить себя им."
+L["idisguise_help_msb1"] = "Возвращает личность игрока или трупа"
+L["idisguise_help_msb2"] = "Переключение между возвращённым и вашей личностью"
+L["idisguise_help_rld"] = "Сбрасывает маскировку личности"
+L["identity_disguiser_new_target"] = "Получен образец игрока {name} и сохранён как цель."
+L["identity_disguiser_hud"] = "{name}"
+L["identity_disguiser_hud_active"] = "{name} (active)"
