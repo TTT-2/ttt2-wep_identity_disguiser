559fb396803e3022eb5741e8ab931036b5d3a375
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-11-01 01:34:59 +0100

    Create identitydisguiser.lua

---
+++ lua/terrortown/lang/french/identitydisguiser.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("french")
+
+L["weapon_indentiy_disguiser_name"] = "Déguisement d'identité"
+L["weapon_indentiy_disguiser_desc"] = "Volez l'identité d'un autre joueur ou d'un cadavre pour prétendre que vous êtes eux."
+L["idisguise_help_msb1"] = "Vole l'identité d'un joueur ou d'un cadavre"
+L["idisguise_help_msb2"] = "Changer entre l'identité volé et votre identité"
+L["idisguise_help_rld"] = "Réinitialisation du déguisement d'identité"
+L["identity_disguiser_new_target"] = "Échantillon de {name} récupéré et stocké comme cible."
+L["identity_disguiser_hud"] = "{name}"
+L["identity_disguiser_hud_active"] = "{name} (active)"
