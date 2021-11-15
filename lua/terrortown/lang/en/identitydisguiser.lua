8ce12124930c9cf7c8828e5ed0429e0f8b1954c3
uenetron <srb5467@gmail.com>
2021-11-15 11:43:04 +0000

    Fix typos (#13)

--- lua/terrortown/lang/en/identitydisguiser.lua
+++ lua/terrortown/lang/en/identitydisguiser.lua
@@ -1,10 +1,10 @@
 L = LANG.GetLanguageTableReference("en")
 
 L["weapon_indentiy_disguiser_name"] = "Identity Disguiser"
-L["weapon_indentiy_disguiser_desc"] = "Retreive the identity of another player or a corpse to pretend you are them."
-L["idisguise_help_msb1"] = "Retreives the identity of a player or a corpse"
-L["idisguise_help_msb2"] = "Toggles between the retreived and your identity"
+L["weapon_indentiy_disguiser_desc"] = "Retrieve the identity of another player or a corpse to pretend you are them."
+L["idisguise_help_msb1"] = "Retrieves the identity of a player or a corpse"
+L["idisguise_help_msb2"] = "Toggles between the retrieved and your identity"
 L["idisguise_help_rld"] = "Resets the identity disguiser"
-L["identity_disguiser_new_target"] = "Retreived sample of {name} and stored as target."
+L["identity_disguiser_new_target"] = "Retrieved sample of {name} and stored as target."
 L["identity_disguiser_hud"] = "{name}"
 L["identity_disguiser_hud_active"] = "{name} (active)"
